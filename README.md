5ea2a56edcc7fb2810469595456707a89550e44d
Gabriel <gabrielp_santos@hotmail.com>
2023-09-27 16:58:05 -0300

    Finalização da calculadora base

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Descrição
 
-- Essa calculadora será feita usando HTML, CSS (Bootstrap) e JavaScript. No momento está em desenvolvimento e esse README será usado para demonstrar o passo a passo que foi usado para desenvolver essa calculadora
 - [Link Bootstrap](https://getbootstrap.com/docs/4.0/components/buttons/)
+- Essa calculadora foi feita usando JavaScript, HTML e CSS (usando o framework Bootstrap para a estilização)
--- index.js
+++ index.js
@@ -1,121 +1,41 @@
-const mostrar = document.querySelector("#painel");
-const operacoes = []; //conjunto que contem as operações
-let conjunto_numeros = []; //conjunto que contem os números
-let conjunto_total = [];
-const mensagem = "Erro";//Mensagem de Error
-let operacao = "(";
+const painel = document.querySelector("#painel");
+const botoes = document.querySelectorAll(".simbolo");
+
+let valor = "";
 let resultado = 0;
 
-function guardar(valor){//Mostra o valor
-    if(mostrar.innerHTML == mensagem) mostrar.innerHTML = '';
-    if(mostrar.innerHTML == resultado) reiniciar();
-    mostrar.innerHTML += `${valor}`;
-}
-
-function reiniciar(){
-    mostrar.innerHTML = '';
-    resultado = 0;
-    operacoes.length = 0;
-    conjunto_numeros.length = 0;
-}
-
-function ponto(){
-    mostrar.innerHTML += '.';
-}
-
-function calculo(conjunto_total){
-    for(i = 0; i<conjunto_total.length; i++){
-        if(i === 0){
-            resultado = conjunto_total[i];
-            operacao += conjunto_total[i];
-        }
-        else{
-            if(i%2 === 0){
-                switch(conjunto_total[i--]){
-                    case "+":
-                        resultado += conjunto_total[i];
-                        break;
-                    case "-":
-                        resultado -= conjunto_total[i];
-                        break;
-                    case "*":
-                        resultado *= conjunto_total[i];
-                        break;
-                    case "/":
-                        resultado /= conjunto_total[i];
-                        break;
-                }
-                operacao += `${conjunto_total[i]}) `;
-            }
-            else{
-                operacao += ` ${conjunto_total[i]} `;
-                continue;
-            }
-        }
-    }
-    conjunto_total = [];
-    operacao += `= ${resultado}`;
-    console.log(operacao);
-    mostrar.innerHTML = resultado;
-}
-
-function processamento(){//Terminar a função de cálculo e ajustar as respostas de output para o usuário
-    if(mostrar.innerHTML !== '')
-        conjunto_total.push(Number.isInteger(mostrar.innerHTML)?Number.parseInt(mostrar.innerHTML):Number.parseFloat(mostrar.innerHTML));
-
-    if(conjunto_numeros.length < 0){
-        mostrar.innerHTML = mensagem;
-        Error("Não tem nenhum valor a ser avaliado");
+function calcular(evento){
+    if(evento.target.value !== "="){
+        valor += evento.target.value;
     }
 
-    if(operacoes.length < 0) return;
+    painel.innerHTML = valor;
 
-    /*conjunto_total = conjunto_total.map(x =>{
-        if(Number.isInteger(x)) Number.parseInt(x);
-        else if(!Number.isNaN(x) && !Number.isInteger(x)) Number.parseFloat(x);
-        else x = x;
-    });*/
-    console.log(conjunto_total);
+    switch(evento.target.value){
+        case "=":
+            resultado = eval(valor);
+            valor = resultado;
+            painel.innerHTML = resultado;
+            break;
+        case "c":
+            valor = "";
+            resultado = 0;
+            console.log(valor.length);
+            painel.innerHTML = 0;
+            break;
+        case "del":
+            valor = valor.slice(0, -4);
+            painel.innerHTML = painel.innerHTML.slice(0, -4);
+            break;
+    }
 
-    calculo(conjunto_total);
+    if(painel.innerHTML === "") painel.innerHTML = "0";
+
+    console.log(valor);
 }
 
-function soma(){
-    operacoes.push("+");
-    conjunto_numeros.push(mostrar.innerHTML);
-    conjunto_total.push(Number.isInteger(mostrar.innerHTML)?Number.parseInt(mostrar.innerHTML):Number.parseFloat(mostrar.innerHTML));
-    conjunto_total.push("+");
-    mostrar.innerHTML = "";
-    console.log(conjunto_numeros);
-    console.log(operacoes);
-}
-
-function subtracao(){
-    operacoes.push("-");
-    conjunto_numeros.push(mostrar.innerHTML);
-    conjunto_total.push(Number.isInteger(mostrar.innerHTML)?Number.parseInt(mostrar.innerHTML):Number.parseFloat(mostrar.innerHTML));
-    conjunto_total.push("-");
-    mostrar.innerHTML = "";
-    console.log(conjunto_numeros);
-    console.log(operacoes);
-}
-
-function multiplicacao(){
-    operacoes.push("*");
-    conjunto_numeros.push(mostrar.innerHTML);
-    conjunto_total.push(Number.isInteger(mostrar.innerHTML)?Number.parseInt(mostrar.innerHTML):Number.parseFloat(mostrar.innerHTML));
-    conjunto_total.push("*");
-    mostrar.innerHTML = "";
-    console.log(conjunto_numeros);
-    console.log(operacoes);
-}
-
-function divisao(){
-    operacoes.push("/");
-    conjunto_numeros.push(mostrar.innerHTML);
-    conjunto_total.push(Number.isInteger(mostrar.innerHTML)?Number.parseInt(mostrar.innerHTML):Number.parseFloat(mostrar.innerHTML));
-    conjunto_total.push("/");
-    mostrar.innerHTML = "";
-    console.log(conjunto_numeros);
-    console.log(operacoes);
-}
+botoes.forEach(botao => {
+    botao.addEventListener("click", evento =>{
+        calcular(evento)
+    })
+})
